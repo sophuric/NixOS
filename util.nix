6529f6294e66ba4859b374899c6efc338feededb
sophur <48314599+sophuric@users.noreply.github.com>
2025-11-02 01:02:22 +1100

    fix(zen-browser): fix invalid function for getting fonts

--- util.nix
+++ util.nix
@@ -4,4 +4,13 @@ args@{ self, ... }:
 with builtins; {
   trimNewlines = x: (replaceStrings [ "\n" "\r" ] [ "" "" ] x);
   first = x: (elemAt x 0);
+  startsWith = string: substring:
+    ((builtins.substring 0 (stringLength substring) string) == string);
+  endsWith = string: substring:
+    (if stringLength string >= stringLength substring then
+      ((builtins.substring (stringLength string - stringLength substring)
+        (stringLength substring) string) == substring)
+    else
+      false);
+  listEquals = list1: list2: (sort lessThan list1) == (sort lessThan list2);
 }
