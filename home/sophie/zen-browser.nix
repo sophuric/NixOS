17e6fe430606c7d67431b1ff9387f099ac58547e
sophur <48314599+sophuric@users.noreply.github.com>
2025-11-07 13:09:27 +1100

    chore(zen-browser): use default/beta instead of twilight

--- home/sophie/zen-browser.nix
+++ home/sophie/zen-browser.nix
@@ -110,7 +110,7 @@ let
   };
 in {
   # catppuccin.zen-browser.enable = true;
-  imports = [ args.zen-browser.homeModules.twilight ];
+  imports = [ args.zen-browser.homeModules.default ];
   home.sessionVariables = {
     MOZ_USE_XINPUT2 = "1";
     MOZ_ENABLE_WAYLAND = "1";
