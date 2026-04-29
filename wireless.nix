4ede9d4c2fc3b7fa51aa17649958730d66b7707d
sophur <48314599+sophuric@users.noreply.github.com>
2026-04-29 20:58:11 +1000

    fix(networking): re-enable scan on low signal, imperatively configure networks now

--- wireless.nix
+++ wireless.nix
@@ -3,13 +3,9 @@
 
 {
   networking.wireless = {
-    scanOnLowSignal = false;
+    scanOnLowSignal = true;
     fallbackToWPA2 = false;
     enable = true;
-    # File should be simple key=value file, psk1=..., etc.
-    # Use wpa_passphrase to generate passphrase
-    secretsFile = "/root/wifi/secret";
-    # I don't care about securing these SSIDs client-side, just from Git
-    networks = import /root/wifi-networks.nix;
+    userControlled.enable = true;
   };
 }
