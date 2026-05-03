f938697757dfe894a3c6c1a02e53e1a55b4656ca
sophur <48314599+sophuric@users.noreply.github.com>
2026-05-03 22:11:48 +1000

    revert: Partially revert "fix(networking): re-enable scan on low signal, imperatively configure networks now"
    This partially reverts commit 4ede9d4c2fc3b7fa51aa17649958730d66b7707d
    to undo imperatively configuring networks

--- wireless.nix
+++ wireless.nix
@@ -7,5 +7,10 @@
     fallbackToWPA2 = false;
     enable = true;
     userControlled.enable = true;
+    # File should be simple key=value file, psk1=..., etc.
+    # Use wpa_passphrase to generate passphrase
+    secretsFile = "/root/wifi/secret";
+    # I don't care about securing these SSIDs client-side, just from Git
+    networks = import /root/wifi-networks.nix;
   };
 }
