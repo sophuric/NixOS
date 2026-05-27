89c6051bafb9a98be30cd6ef426e496e2e57c12a
sophur <48314599+sophuric@users.noreply.github.com>
2026-05-27 15:03:08 +1000

    feat(vesktop): enable PlainFolderIcon

--- home/vesktop.nix
+++ home/vesktop.nix
@@ -464,6 +464,7 @@
               "DisableDeepLinks"
               "ExpressionCloner"
               "CopyStickerLinks"
+              "PlainFolderIcon"
             ] (x: { enabled = true; }))
           ];
           notifications = {
